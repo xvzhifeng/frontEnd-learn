dd19ae7d5ca6c5f31507e0599d71cd0f6168071d
xzf <xvzhifeng@126.com>
2022-06-17 15:51:42 +0800

    next

--- next.js/hello-world/nextjs-blog/README.md
+++ next.js/hello-world/nextjs-blog/README.md
@@ -1,1 +1,9 @@
+# Next.js Hello world
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+## 前言：
+**涉及到的模块**
+
+- npm install remark remark-html
+- npm install gray-matter
+- npm install date-fns
--- next.js/hello-world/nextjs-blog/pages/index.js
+++ next.js/hello-world/nextjs-blog/pages/index.js
@@ -1,8 +1,11 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -15,6 +18,31 @@ export default function Home() {
           <a href="https://www.nextjs.cn/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
