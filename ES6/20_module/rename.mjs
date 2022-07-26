6de214e859b217537c04ed2ee7a30242600805dd
xzf <xvzhifeng@126.com>
2022-07-27 00:19:41 +0800

    add

--- ES6/20_module/rename.mjs
+++ ES6/20_module/rename.mjs
@@ -1,6 +1,10 @@
 import { test,rename,test6_name } from "./test6.mjs"
 // import { test,x } from './test6.mjs'
+let t_name = test6_name
 test()
 console.log(test6_name)
 rename("rename")
 console.log(test6_name) // rename
+test6_name
+console.log("t_name a" + t_name)
+test()
