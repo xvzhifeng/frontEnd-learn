c5d75f3ab345abf7e58adf1ae107edf2d118b7a7
xuzhif <xvzhifeng@126.com>
2022-11-19 17:59:47 +0000

    add vue-base-demo-01

---
+++ 01-vue/01-base-demo/base-demo/src/components/HelloVue.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import type { Ref } from 'vue'
+import router from "../router/index"
+let message = ref<string>('Hello Vue')
+let username: Ref<string> = ref('')
+let password: Ref<String> = ref('')
+let status: Ref<string> = ref('')
+let isRed = ref(true)
+let already_login_user: Ref<string[]> = ref([])
+let change = () => {
+    if (message.value == 'Hello world') {
+        message.value = 'Hello Vue'
+    } else {
+        message.value = 'Hello world'
+    }
+
+    console.log("click")
+}
+
+function toggleRed() {
+    isRed.value = !isRed.value
+}
+
+function login() {
+    if (already_login_user.value.includes(username.value)) {
+        status.value = 'already login'
+        return
+    }
+    if (username.value != "" && password.value == "123456") {
+        status.value = 'validate'
+        message.value = `hello ${username.value}`
+        // router.go("/")
+        already_login_user.value.push(username.value)
+    } else {
+        status.value = 'invalidate'
+    }
+}
+
+function rm_login_user(user: string) {
+    console.log(user)
+    already_login_user.value = already_login_user.value.filter((x) => {
+        return x != user
+    })
+}
+watch(status, (newStatus, oldStatus)=>{
+    console.log(newStatus)
+    console.log(oldStatus)
+})
+
+/*
+    watch 监控对于引用类型，无法获取前后的值，如下list所示，new 和 old 会输出一样的值。
+*/
+watch(already_login_user.value, (newAlready_login_user, old)=>{
+    console.log(newAlready_login_user)
+    console.log(old)
+})
+
+
+</script>
+
+<template>
+    <div @keydown.enter="login">
+        <h1 @click="change">{{ message }}</h1>
+        <div>
+            <span> username: </span>
+            <input v-model="username" id="input_01" @change="toggleRed" />
+        </div>
+        <div>
+            <span>password: </span>
+            <input v-model="password" />
+        </div>
+        <input type="button" value="login" @click="login" />
+        <p :class="{ red: isRed }" @click="toggleRed"> login status: {{ status }}</p>
+        <ul v-if="already_login_user.length">
+            <li v-for="user of already_login_user">
+                {{ user }}
+                <button @click="rm_login_user(user)">X</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style>
+.red {
+    color: red;
+}
+</style>
---
+++ 01-vue/01-base-demo/base-demo/src/views/AboutView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import {ref} from 'vue'
+let count = ref(0)
+let increase = () => {
+  console.log("test")
+  count.value += 1
+}
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h1 @click="() => {count =count + 1}">{{count}}</h1>
+    <button @click="increase"> test </button>
+  </div>
+  
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
